72b916235262ebc5df9020fedb1e97dfa5153fc6
Alex Mihov <mihov.alex@gmail.com>
2015-05-09 19:38:37 +0200

    [FEATURE] Added "Marktplatz" and additional funktions
    The Marktplatz tab:
    - Displays the defined Entries
    - Links automatically to the entry

---
+++ App/ToniApp/www/js/controllers.js
@@ -0,0 +1,31 @@
+angular.module('ToniApp.controllers', [])
+
+.controller('MarketCtrl', function($scope, MarketEntries) {
+    $scope.marketEntries = MarketEntries.getAll();
+})
+
+.controller('MarketDetailCtrl', function($scope, $stateParams, MarketEntries) {
+	var currentID = $stateParams.marketEntryID;
+    $scope.marketEntry = MarketEntries.get(currentID);
+    $scope.remove = function(chat) {
+        Chats.remove(chat);
+    }
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function(chat) {
+        Chats.remove(chat);
+    }
+})
+
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
+});
