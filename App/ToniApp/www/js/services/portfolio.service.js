7532b171998dec330d3ad8e70d735212938171b7
Alex Mihov <mihov.alex@gmail.com>
2015-05-17 01:10:06 +0200

    [UPDATE] portfolios: added mappings between creators and portfolios;

--- App/ToniApp/www/js/services/portfolio.service.js
+++ App/ToniApp/www/js/services/portfolio.service.js
@@ -6,49 +6,49 @@ angular.module('ToniApp.services')
     // Some fake testing data
     var portfolios = [{
         id: 0,
-        name: 'Maria Feltscher',
+        creator_id: 1,
         project_title: 'Doh-Lock',
         project_tags:'Konzept, Design',
         picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png'
     }, {
         id: 1,
-        name: 'Benjamin Sulser',
+        creator_id: 3,
         project_title: 'Luzerner Bier Redesign',
         project_tags:'Grafik',
         picture: 'http://www.wineware.co.uk/content/images/thumbs/0010058_schott-zwiesel-beer-basic-small-beer-glasses-set-of-6.jpeg'
     }, {
         id: 2,
-        name: 'Luca Siebner',
+        creator_id: 2,
         project_title: 'Jazz Konzert',
         project_tags:'Musik, Jazz',
         picture: 'http://www.audiophileusa.com/covers400water/23108.jpg'
     }, {
         id: 3,
-        name: 'Ana Minovic',
+        creator_id: 4,
         project_title: 'Mode Kollektion',
         project_tags:'Konzept, Kleidung',
         picture: 'http://fashionsup.com/wp-content/uploads/2012/05/how-to-draw-fashion-sketches.jpg'
     }, {
         id: 4,
-        name: 'Nicolaus Fehr',
+        creator_id: 5,
         project_title: 'Pop-Art Ausstellung',
         project_tags:'Kunst, Pop-Art',
         picture: 'http://art-canyon.com/pop-art/wp-content/uploads/2010/08/james-dean-pop-art.jpg'
     }, {
         id: 5,
-        name: 'Frederike Gruber',
+        creator_id: 6,
         project_title: 'Retro Werbung',
         project_tags:'Film, Werbung, Retro',
         picture: 'https://www.tradebit.com/usr/beulah/pub/9002/8552600_332smx.jpg'
     }, {
         id: 6,
-        name: 'Tom Spycher',
+        creator_id: 7,
         project_title: 'Ballet Abend',
         project_tags:'Tanzen, Ballet',
         picture: 'http://bachtrack.com/files/1097-1498-marianela_nunez_and_thiago_soares_in_swan_lake._photo_alice_pennefather__courtesy_of_roh.jpg'
     }, {
         id: 7,
-        name: 'Martin Wey',
+        creator_id: 8,
         project_title: 'Ringo',
         project_tags:'Western, Film',
         picture: 'http://i.imgur.com/u8hiSPa.jpg'
