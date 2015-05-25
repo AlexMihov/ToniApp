7d70eca505e558e8723fa9b6a08d024880c0d737
Alex Mihov <mihov.alex@gmail.com>
2015-05-25 19:55:25 +0200

    [CHORE] Filter: added correct Departments to filters;

--- App/ToniApp/www/js/services/market.filter.service.js
+++ App/ToniApp/www/js/services/market.filter.service.js
@@ -41,27 +41,107 @@ angular.module('ToniApp.services')
             items: [{
                 active: true,
                 isDivider: true,
-                name: 'Design'
+                name: 'DDE'
             },{
                 active: true,
                 isDivider: false,
-                name: 'Cast'
+                name: 'CAST/Audiovisuelle Medien'
+            },{
+                active: true,
+                isDivider: false,
+                name: 'Game Design'
+            },{
+                active: true,
+                isDivider: false,
+                name: 'Industrial Design'
             },{
                 active: true,
                 isDivider: false,
                 name: 'Interaction Design'
             },{
                 active: true,
-                isDivider: true,
-                name: 'Kunst'
+                isDivider: false,
+                name: 'Scientific Visualisation'
             },{
+                active: true,
+                isDivider: false,
+                name: 'Style & Design'
+            },{
+                active: true,
+                isDivider: false,
+                name: 'Visuelle Kommunikation'
+            } ,{
+                active: true,
+                isDivider: true,
+                name: 'DDK'
+            } ,{
+                active: true,
+                isDivider: false,
+                name: 'Film'
+            } ,{
+                active: true,
+                isDivider: false,
+                name: 'Tanz'
+            } ,{
+                active: true,
+                isDivider: false,
+                name: 'Theater'
+            } ,{
+                active: true,
+                isDivider: true,
+                name: 'DKM'
+            } ,{
+                active: true,
+                isDivider: false,
+                name: 'Bildende Kunst'
+            } ,{
                 active: true,
                 isDivider: false,
                 name: 'Fotografie'
-            },{
+            } ,{
                 active: true,
                 isDivider: false,
-                name: 'Bildhauerei'
+                name: 'Mediale Künste'
+            } ,{
+                active: true,
+                isDivider: true,
+                name: 'DKV'
+            } ,{
+                active: true,
+                isDivider: false,
+                name: 'Art Education'
+            } ,{
+                active: true,
+                isDivider: true,
+                name: 'DMU'
+            } ,{
+                active: true,
+                isDivider: false,
+                name: 'Jazz & Pop'
+            } ,{
+                active: true,
+                isDivider: false,
+                name: 'Kirchenmusik'
+            } ,{
+                active: true,
+                isDivider: false,
+                name: 'Klassik'
+            } ,{
+                active: true,
+                isDivider: false,
+                name: 'Komposition'
+            } ,{
+                active: true,
+                isDivider: false,
+                name: 'Musik und Bewegun'
+            } ,{
+                active: true,
+                isDivider: false,
+                name: 'Schulmusik'
+            } ,{
+                active: true,
+                isDivider: false,
+                name: 'Tonmeister'
             }]
         },{
             id: 3,
