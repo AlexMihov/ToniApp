d0a4d8b0a9449258a58c42938c35ff4d66925cc0
Mattias Bush <mattias.bush@gmail.com>
2015-05-25 22:41:18 +0200

    [update] portfolio projects

--- App/ToniApp/www/js/services/portfolio.service.js
+++ App/ToniApp/www/js/services/portfolio.service.js
@@ -35,7 +35,7 @@ angular.module('ToniApp.services')
             url: 'img/portfolio/Pictures/project_1.jpg'
         },
         paragraphs: [{
-            title: 'Innen/Aussen',
+            title: '',
             content: 'In einem Fotoprojekt in Vézelay Burgund arbeiteten wir während zwei Wochen an einem fotografischen Thema, welches mit Architektur zutun hatte. Innen und Aussenwelten flossen ineinander und liessen neue Räume entstehen.'
         }]
     }, {
@@ -49,7 +49,7 @@ angular.module('ToniApp.services')
             url: 'img/portfolio/Pictures/project_2.jpg'
         },
         paragraphs: [{
-            title: 'Austellung im Feuerwehrdepo in Rapperswil',
+            title: '',
             content: 'Für meine Fotodokumentation „Lichtstreifen“ fotografierte ich mit einer analogen Spiegelreflexkamera während einer Woche Innenräume. Ich experimentierte mit verschiedenen  Lichtquellen. Im Fotolabor entwickelte ich meine Bilder und vergrösserte sie auf Grossformat.',
             media: {
                 type: 'picture',
@@ -60,7 +60,7 @@ angular.module('ToniApp.services')
         id: 3,
         creator_id: 4,
         project_title: 'Schwarz ist mehr als eine Farbe',
-        project_tags: 'Konzept, Kleidung',
+        project_tags: 'Kleidung',
         picture: 'img/portfolio/Pictures/project_3.jpg',
         banner: {
             type: 'picture',
@@ -68,52 +68,52 @@ angular.module('ToniApp.services')
         },
         paragraphs: [{
             title: 'Schwarz ist mehr als eine Farbe',
-            content: 'In einem interdisziplinären Modul  in Stile und Design untesuchten Jan, Fabienne und ich die Bedeutung der Farbe „ Schwarz „ in der Mode der letzten 100 Jahre.  Mit Recherchen und einer eigenen Dokumentation zur Bedeutung black fashion wanderte unsere Arbeit sogar nach Berlin, auf was wir natürlich sehr stolz sind.'
+            content: 'In einem interdisziplinären Modul  in Style & Design untesuchten Jan, Fabienne und ich die Bedeutung der Farbe „ Schwarz „ in der Mode der letzten 100 Jahre.  Mit Recherchen und einer eigenen Dokumentation zur Bedeutung black fashion wanderte unsere Arbeit sogar nach Berlin, auf was wir natürlich sehr stolz sind.'
         }]
     }, {
         id: 4,
         creator_id: 5,
-        project_title: 'Pop-Art Ausstellung',
-        project_tags: 'Kunst, Pop-Art',
-        picture: 'http://art-canyon.com/pop-art/wp-content/uploads/2010/08/james-dean-pop-art.jpg',
+        project_title: 'Birds',
+        project_tags: 'Film',
+        picture: 'img/portfolio/Pictures/project_4.jpg',
         banner: {
             type: 'picture',
-            url: 'http://d1oi7t5trwfj5d.cloudfront.net/fe/08/fbd7e11241fd977d6f83e2f3a675/homer.jpg'
+            url: 'img/portfolio/Pictures/project_4.jpg'
         },
         paragraphs: [{
-            title: '',
-            content: ''
+            title: 'Birds',
+            content: 'Für unseren Krimi-Kurzlfim „ birds“ , eine Anlehnung an Hitchkock ,drehten wir vor allem an den Ufern des Zürichsees. Das Krimidrama handelt von Janez, einem passlosen Slowenier, der von Halluzinationen geplagt immer wieder Dinge sah, die einmal Wirklichkeit waren. Sein zweites ich lebte in einer anderen Zeit.'
         }]
     }, {
         id: 5,
         creator_id: 6,
-        project_title: 'Retro Werbung',
-        project_tags: 'Film, Werbung, Retro',
-        picture: 'https://www.tradebit.com/usr/beulah/pub/9002/8552600_332smx.jpg',
+        project_title: 'Schnipp Schnapp',
+        project_tags: 'Bild, Fotografie',
+        picture: 'img/portfolio/Pictures/project_5.jpg',
         banner: {
             type: 'picture',
-            url: 'http://d1oi7t5trwfj5d.cloudfront.net/fe/08/fbd7e11241fd977d6f83e2f3a675/homer.jpg'
+            url: 'img/portfolio/Pictures/project_5.jpg'
         },
         paragraphs: [{
-            title: '',
-            content: ''
+            title: 'Schnipp Schnapp',
+            content: 'In unserem letzten Z- Modul zum Thema „ Wahrnehmung“ untersuchten wir die selektive Wahrnehmung von Bildern. Wir verglichen Schnappschüsse und analysierten verborgene Inhalte. '
         }]
     }, {
         id: 6,
         creator_id: 1,
-        project_title: 'Doh-Lock',
-        project_tags: 'Konzept, Design',
+        project_title: 'Bildlügen oder betrügerische Absichten',
+        project_tags: 'Analyse, Bild',
         picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
         banner: {
-            type: 'video',
-            url: 'https://www.youtube.com/embed/PulHR2LfPVk'
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_6.jpg'
         },
         paragraphs: [{
-            title: '',
-            content: 'Als langjährige Liebhaberin von den Simpsons, wollte ich umbedingt ein Produkt entwerfen, welches den Humor und Stil der Simpsons beinhaltet. Der Doh-Lock nimmt Homers beliebten Spruch und macht aus ihm ein einzigartiges Schloss, welches jeden zum Schmunzeln bringen wird.',
+            title: 'Bildlügen oder betrügerische Absichten',
+            content: 'Beim Hausräumen von Andreas Grossmutter fanden wir Schachteln voll mit Postkarten der 20er 30er und 40er Jahre.  Wie inventarisierten, ordneten nach Kriterien analysierten und untersuchten manipuliertes Material hin zu neuen Bildaussagen.  ',
             media: {
                 type: 'picture',
-                url: ''
+                url: 'img/portfolio/Pictures/project_6.jpg'
             }
         }, {
             title: 'Paragraph Nr2',
@@ -122,16 +122,271 @@ angular.module('ToniApp.services')
     }, {
         id: 7,
         creator_id: 8,
-        project_title: 'Ringo',
-        project_tags: 'Western, Film',
-        picture: 'http://i.imgur.com/u8hiSPa.jpg',
+        project_title: 'Vom Element zum System',
+        project_tags: 'Objekt, Körper',
+        picture: 'img/portfolio/Pictures/project_7.jpg',
         banner: {
             type: 'picture',
-            url: 'http://d1oi7t5trwfj5d.cloudfront.net/fe/08/fbd7e11241fd977d6f83e2f3a675/homer.jpg'
+            url: 'img/portfolio/Pictures/project_7.jpg'
         },
         paragraphs: [{
+            title: 'Vom Element zum System',
+            content: 'In diesen Workshop wurden aus  simplen Objeken, wie Wäscheklammern,  Fadenspulen, durch neue Anordnungen und Kombinationen skurrile Welten erschafft und fotografisch festgehalten. Körper und Raumgefüge waren einem ständigen Veränderungsprozess unterworfen.'
+        }]
+    }, {
+        id: 8,
+        creator_id: 1,
+        project_title: 'Paper Fashion',
+        project_tags: 'Mode, Papier, Fotoshooting',
+        picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
+        banner: {
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_8.jpg'
+        },
+        paragraphs: [{
+            title: 'Paper Fashion',
+            content: 'In diesem Wochen Modul  in Stile und Design erlernten wir die verschiedensten Paierbearbeitungstechniken wie Wabetechnik, Knüllen, Schneiden, Falten... Wir machten Materialproben,
+gestalterische Experimente und entwickelten schlussendlich einen Prototypen. 
+Anhand von Schneiderpuppen wurden unsere Ideen
+konkret umgesetzt und in einem Fotoshooting abgerundet.',
+            media: {
+                type: 'picture',
+                url: 'img/portfolio/Pictures/project_8.jpg'
+            }
+        }, {
             title: '',
             content: ''
+        }]
+            }, {
+        id: 9,
+        creator_id: 1,
+        project_title: 'Kleider machen Musik',
+        project_tags: 'Analyse, Bild',
+        picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
+        banner: {
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_9.jpg'
+        },
+        paragraphs: [{
+            title: 'Bildlügen oder betrügerische Absichten',
+            content: 'Endlich ist es soweit. Nadia und ich haben’s es geschafft!! Unser Modeprojekt „ Modekollektion zum Thema Klang“ aus Aluminium, Acrylglas und Seide , kann nun doch noch, nach langem Proben und Bangen, realisiert werden. Wir freuen uns auf euch. Morgen 6. Juni. 2015. Ausstellung im Raum 4K.22.',
+            media: {
+                type: 'picture',
+                url: 'img/portfolio/Pictures/project_9.jpg'
+            }
+        }, {
+            title: '',
+            content: ''
+        }]
+            }, {
+        id: 6,
+        creator_id: 1,
+        project_title: 'Bildlügen oder betrügerische Absichten',
+        project_tags: 'Analyse, Bild',
+        picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
+        banner: {
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_6.jpg'
+        },
+        paragraphs: [{
+            title: 'Bildlügen oder betrügerische Absichten',
+            content: 'Beim Hausräumen von Andreas Grossmutter fanden wir Schachteln voll mit Postkarten der 20er 30er und 40er Jahre.  Wie inventarisierten, ordneten nach Kriterien analysierten und untersuchten manipuliertes Material hin zu neuen Bildaussagen.  ',
+            media: {
+                type: 'picture',
+                url: 'img/portfolio/Pictures/project_6.jpg'
+            }
+        }, {
+            title: 'Paragraph Nr2',
+            content: 'Dies ist nun ein zweiter, sehr wichtiger Abschnitt, da ich noch mehr testen muss. Ich will sehen, ob das Programm alle Paragraphen dynamisch anzeigt.'
+        }]
+            }, {
+        id: 6,
+        creator_id: 1,
+        project_title: 'Bildlügen oder betrügerische Absichten',
+        project_tags: 'Analyse, Bild',
+        picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
+        banner: {
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_6.jpg'
+        },
+        paragraphs: [{
+            title: 'Bildlügen oder betrügerische Absichten',
+            content: 'Beim Hausräumen von Andreas Grossmutter fanden wir Schachteln voll mit Postkarten der 20er 30er und 40er Jahre.  Wie inventarisierten, ordneten nach Kriterien analysierten und untersuchten manipuliertes Material hin zu neuen Bildaussagen.  ',
+            media: {
+                type: 'picture',
+                url: 'img/portfolio/Pictures/project_6.jpg'
+            }
+        }, {
+            title: 'Paragraph Nr2',
+            content: 'Dies ist nun ein zweiter, sehr wichtiger Abschnitt, da ich noch mehr testen muss. Ich will sehen, ob das Programm alle Paragraphen dynamisch anzeigt.'
+        }]
+            }, {
+        id: 6,
+        creator_id: 1,
+        project_title: 'Bildlügen oder betrügerische Absichten',
+        project_tags: 'Analyse, Bild',
+        picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
+        banner: {
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_6.jpg'
+        },
+        paragraphs: [{
+            title: 'Bildlügen oder betrügerische Absichten',
+            content: 'Beim Hausräumen von Andreas Grossmutter fanden wir Schachteln voll mit Postkarten der 20er 30er und 40er Jahre.  Wie inventarisierten, ordneten nach Kriterien analysierten und untersuchten manipuliertes Material hin zu neuen Bildaussagen.  ',
+            media: {
+                type: 'picture',
+                url: 'img/portfolio/Pictures/project_6.jpg'
+            }
+        }, {
+            title: 'Paragraph Nr2',
+            content: 'Dies ist nun ein zweiter, sehr wichtiger Abschnitt, da ich noch mehr testen muss. Ich will sehen, ob das Programm alle Paragraphen dynamisch anzeigt.'
+        }]
+            }, {
+        id: 6,
+        creator_id: 1,
+        project_title: 'Bildlügen oder betrügerische Absichten',
+        project_tags: 'Analyse, Bild',
+        picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
+        banner: {
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_6.jpg'
+        },
+        paragraphs: [{
+            title: 'Bildlügen oder betrügerische Absichten',
+            content: 'Beim Hausräumen von Andreas Grossmutter fanden wir Schachteln voll mit Postkarten der 20er 30er und 40er Jahre.  Wie inventarisierten, ordneten nach Kriterien analysierten und untersuchten manipuliertes Material hin zu neuen Bildaussagen.  ',
+            media: {
+                type: 'picture',
+                url: 'img/portfolio/Pictures/project_6.jpg'
+            }
+        }, {
+            title: 'Paragraph Nr2',
+            content: 'Dies ist nun ein zweiter, sehr wichtiger Abschnitt, da ich noch mehr testen muss. Ich will sehen, ob das Programm alle Paragraphen dynamisch anzeigt.'
+        }]
+            }, {
+        id: 6,
+        creator_id: 1,
+        project_title: 'Bildlügen oder betrügerische Absichten',
+        project_tags: 'Analyse, Bild',
+        picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
+        banner: {
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_6.jpg'
+        },
+        paragraphs: [{
+            title: 'Bildlügen oder betrügerische Absichten',
+            content: 'Beim Hausräumen von Andreas Grossmutter fanden wir Schachteln voll mit Postkarten der 20er 30er und 40er Jahre.  Wie inventarisierten, ordneten nach Kriterien analysierten und untersuchten manipuliertes Material hin zu neuen Bildaussagen.  ',
+            media: {
+                type: 'picture',
+                url: 'img/portfolio/Pictures/project_6.jpg'
+            }
+        }, {
+            title: 'Paragraph Nr2',
+            content: 'Dies ist nun ein zweiter, sehr wichtiger Abschnitt, da ich noch mehr testen muss. Ich will sehen, ob das Programm alle Paragraphen dynamisch anzeigt.'
+        }]
+            }, {
+        id: 6,
+        creator_id: 1,
+        project_title: 'Bildlügen oder betrügerische Absichten',
+        project_tags: 'Analyse, Bild',
+        picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
+        banner: {
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_6.jpg'
+        },
+        paragraphs: [{
+            title: 'Bildlügen oder betrügerische Absichten',
+            content: 'Beim Hausräumen von Andreas Grossmutter fanden wir Schachteln voll mit Postkarten der 20er 30er und 40er Jahre.  Wie inventarisierten, ordneten nach Kriterien analysierten und untersuchten manipuliertes Material hin zu neuen Bildaussagen.  ',
+            media: {
+                type: 'picture',
+                url: 'img/portfolio/Pictures/project_6.jpg'
+            }
+        }, {
+            title: 'Paragraph Nr2',
+            content: 'Dies ist nun ein zweiter, sehr wichtiger Abschnitt, da ich noch mehr testen muss. Ich will sehen, ob das Programm alle Paragraphen dynamisch anzeigt.'
+        }]
+            }, {
+        id: 6,
+        creator_id: 1,
+        project_title: 'Bildlügen oder betrügerische Absichten',
+        project_tags: 'Analyse, Bild',
+        picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
+        banner: {
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_6.jpg'
+        },
+        paragraphs: [{
+            title: 'Bildlügen oder betrügerische Absichten',
+            content: 'Beim Hausräumen von Andreas Grossmutter fanden wir Schachteln voll mit Postkarten der 20er 30er und 40er Jahre.  Wie inventarisierten, ordneten nach Kriterien analysierten und untersuchten manipuliertes Material hin zu neuen Bildaussagen.  ',
+            media: {
+                type: 'picture',
+                url: 'img/portfolio/Pictures/project_6.jpg'
+            }
+        }, {
+            title: 'Paragraph Nr2',
+            content: 'Dies ist nun ein zweiter, sehr wichtiger Abschnitt, da ich noch mehr testen muss. Ich will sehen, ob das Programm alle Paragraphen dynamisch anzeigt.'
+        }]
+            }, {
+        id: 6,
+        creator_id: 1,
+        project_title: 'Bildlügen oder betrügerische Absichten',
+        project_tags: 'Analyse, Bild',
+        picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
+        banner: {
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_6.jpg'
+        },
+        paragraphs: [{
+            title: 'Bildlügen oder betrügerische Absichten',
+            content: 'Beim Hausräumen von Andreas Grossmutter fanden wir Schachteln voll mit Postkarten der 20er 30er und 40er Jahre.  Wie inventarisierten, ordneten nach Kriterien analysierten und untersuchten manipuliertes Material hin zu neuen Bildaussagen.  ',
+            media: {
+                type: 'picture',
+                url: 'img/portfolio/Pictures/project_6.jpg'
+            }
+        }, {
+            title: 'Paragraph Nr2',
+            content: 'Dies ist nun ein zweiter, sehr wichtiger Abschnitt, da ich noch mehr testen muss. Ich will sehen, ob das Programm alle Paragraphen dynamisch anzeigt.'
+        }]
+            }, {
+        id: 6,
+        creator_id: 1,
+        project_title: 'Bildlügen oder betrügerische Absichten',
+        project_tags: 'Analyse, Bild',
+        picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
+        banner: {
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_6.jpg'
+        },
+        paragraphs: [{
+            title: 'Bildlügen oder betrügerische Absichten',
+            content: 'Beim Hausräumen von Andreas Grossmutter fanden wir Schachteln voll mit Postkarten der 20er 30er und 40er Jahre.  Wie inventarisierten, ordneten nach Kriterien analysierten und untersuchten manipuliertes Material hin zu neuen Bildaussagen.  ',
+            media: {
+                type: 'picture',
+                url: 'img/portfolio/Pictures/project_6.jpg'
+            }
+        }, {
+            title: 'Paragraph Nr2',
+            content: 'Dies ist nun ein zweiter, sehr wichtiger Abschnitt, da ich noch mehr testen muss. Ich will sehen, ob das Programm alle Paragraphen dynamisch anzeigt.'
+        }]
+            }, {
+        id: 6,
+        creator_id: 1,
+        project_title: 'Bildlügen oder betrügerische Absichten',
+        project_tags: 'Analyse, Bild',
+        picture: 'http://2.bp.blogspot.com/-y8yEusAKcUM/TaoLBwfTQpI/AAAAAAAABAw/rHpOeL4OFMY/s1600/homer-simpson.png',
+        banner: {
+            type: 'picture',
+            url: 'img/portfolio/Pictures/project_6.jpg'
+        },
+        paragraphs: [{
+            title: 'Bildlügen oder betrügerische Absichten',
+            content: 'Beim Hausräumen von Andreas Grossmutter fanden wir Schachteln voll mit Postkarten der 20er 30er und 40er Jahre.  Wie inventarisierten, ordneten nach Kriterien analysierten und untersuchten manipuliertes Material hin zu neuen Bildaussagen.  ',
+            media: {
+                type: 'picture',
+                url: 'img/portfolio/Pictures/project_6.jpg'
+            }
+        }, {
+            title: 'Paragraph Nr2',
+            content: 'Dies ist nun ein zweiter, sehr wichtiger Abschnitt, da ich noch mehr testen muss. Ich will sehen, ob das Programm alle Paragraphen dynamisch anzeigt.'
         }]
     }];
 
