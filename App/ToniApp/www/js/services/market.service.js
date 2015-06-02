c8f02de09deb866f470386048a236a4652ff14f4
Alex Mihov <mihov.alex@gmail.com>
2015-06-02 04:38:20 +0200

    [CHORE] market: changed the creators of the market entries to fit their profiles;

--- App/ToniApp/www/js/services/market.service.js
+++ App/ToniApp/www/js/services/market.service.js
@@ -9,35 +9,35 @@ angular.module('ToniApp.services')
             content_long: 'Für meinen Kurzfilm "Der toxische Morgen" suche ich unbedingt noch einen Schauspieler für die Rolle eines Jägers. Schaupielerfahrung wär erwünscht, Alter bevorzugt zwischen 30-40, männlich',
             tags: ['Video', 'Film'],
             timespan: '3 Drehtage, voraussichtlich Mitte Mai',
-            creator_id: 1
+            creator_id: 19
         }, {
             id: 1,
             title: 'Tänzer für Tanzshow YIKYAK',
             content_long: 'Hast du Freude an Popmusik und du tanzt gerne? Für unsere Performance brauchen wir noch zwei männliche Tänzer, welche sehr gut breakdancen können. Sie werden im Hitergrund der Vorstellung für den zusätzliche WOW-Effekt sorgen und einige sehr komplizierte Moves ausführen müssen.',
             tags: ['Tanz'],
             timespan: 'Jedes Wochenende im Juni, 2015',
-            creator_id: 2
+            creator_id: 15
         }, {
             id: 2,
             title: 'Suche erfahrenen Fotografen',
             content_long: 'Für meine neue Kleiderkollektion erstelle ich ein Fashion Lookbook. Gibt es einen Fotografen, der Erfahrung mit Shootings hat und mir gerne für eine sehr kleine Entlöhnung helfen will. Es werden professionelle Models anwesend sein, welche schon für grosse Kleiderlabels gearbeitet haben.',
             tags: ['Fotografie'],
             timespan: 'Jedes Wochenende im Juni, 2015',
-            creator_id: 3
+            creator_id: 23
         }, {
             id: 3,
             title: 'Brauche Hilfe bei After Effects',
             content_long: 'Ich versuche eine tolle Animation zu machen, jedoch kenne ich mich nicht so gut aus mit After Effects CS6 und brauche dringend Hilfe von jemandem. Bei meinem Problem geht es um verschiede Verhalten bei Partikeln, welche sich physikalisch korrekt verhalten sollten.',
             tags: ['After Effects'],
             timespan: 'Jedes Wochenende im Juni, 2015',
-            creator_id: 4
+            creator_id: 36
         }, {
             id: 4,
             title: 'Webentwickler für Webshop',
             content_long: 'Ich habe ein eigenes Kleiderlabel entworfen und will meine Kleider in einem Webshop verkaufen. Suche dringend einen guten Fotografen. Ich wäre sehr froh, wenn du mir kurz deine Refferenzen schicken könntest in Form von einem Link. Ich werde sie mir ansehen und dich danach kontaktieren.',
             tags: ['Web','Informatik',' Design'],
             timespan: 'Jedes Wochenende im Juni, 2015',
-            creator_id: 5
+            creator_id: 54
         }];
 
         return {
